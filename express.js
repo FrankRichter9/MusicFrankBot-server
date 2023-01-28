56e748387ceac04146ebdaeb30eca67567beb7b0
FrankRichter9 <55855672+FrankRichter9@users.noreply.github.com>
2023-01-28 23:40:15 +0300

    add socket test

--- express.js
+++ express.js
@@ -1,19 +1,27 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-
+import { Server } from 'socket.io'
+import http from 'http'
 import { createClient } from 'redis';
-console.log('process.env.REDIS_HOST', process.env.REDIS_HOST)
-const client = createClient({ url: 'redis://redis:6379' });
-
-client.on('error', (err) => console.log('Redis Client Error', err));
-
-await client.connect();
-
-const publisher = client.duplicate();
-await publisher.connect();
 
 const app = express()
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }})
+
+// const client = createClient({ url: 'redis://redis:6379' });
+
+// client.on('error', (err) => console.log('Redis Client Error', err));
+
+// await client.connect();
+
+// const publisher = client.duplicate();
+// await publisher.connect();
+
+
 
 app.use(cors())
 
@@ -61,4 +69,18 @@ async function videos(search) {
     return result.videos.slice(0, 5)
 }
 
-app.listen(4000)
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('data', (msg) => {
+    console.log('message: ' + JSON.stringify(msg));
+    });
+});
+
+
+
+
+
+// app.listen(4000)
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
